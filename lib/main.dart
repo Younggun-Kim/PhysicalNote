a8e4ad1eb16158a5ab8fb2568431d072ac31320f
Conner Kim <dudrjs145@naver.com>
2023-12-10 19:37:57 +0900

    Locale 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:get/get.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:physical_note/app/config/initializer/app_initializer.dart';
@@ -21,7 +22,6 @@ void run() async {
 
   // initTheme();
 
-
   await AppInitializer.init();
 
   // CERTIFICATE_VERIFY_FAILED 방지.
@@ -30,22 +30,33 @@ void run() async {
   // Locale 초기화.
   await initializeDateFormatting();
 
-  runApp(GetMaterialApp(
-    debugShowCheckedModeBanner: false,
-    initialRoute: RouteType.SPLASH,
-    // initialRoute: RouteType.TEST,
-    defaultTransition: Transition.fade,
-    getPages: Routes.pages,
-    translations: AppTranslations(),
-    locale: Get.deviceLocale,
-    fallbackLocale: const Locale('ko', 'KR'), // Default Locale
-  ));
+  runApp(
+    GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: RouteType.SPLASH,
+      // initialRoute: RouteType.TEST,
+      defaultTransition: Transition.fade,
+      getPages: Routes.pages,
+      translations: AppTranslations(),
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('ko', 'KR'), // Default Locale
+        Locale('en', 'US'),
+      ],
+      locale: Get.deviceLocale,
+    ),
+  );
 }
 
-class MyHttpOverrides extends HttpOverrides{
+class MyHttpOverrides extends HttpOverrides {
   @override
-  HttpClient createHttpClient(SecurityContext? context){
+  HttpClient createHttpClient(SecurityContext? context) {
     return super.createHttpClient(context)
-      ..badCertificateCallback = (X509Certificate cert, String host, int port)=> true;
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
   }
 }
