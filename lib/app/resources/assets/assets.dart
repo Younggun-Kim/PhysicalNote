45abb3c1e7b423e62027346341b87ee1edb007a5
Conner Kim <dudrjs145@naver.com>
2024-01-21 18:28:22 +0900

    사람 앞 모습 id 작업.

--- lib/app/resources/assets/assets.dart
+++ lib/app/resources/assets/assets.dart
@@ -17,6 +17,7 @@ class Assets {
   static const String foot = 'assets/images/foot.svg';
   static const String hand = 'assets/images/hand.svg';
   static const String hand2 = 'assets/images/hand2.png';
+  static const String humanFront = 'assets/images/human_front.svg';
   static const String icChevronright = 'assets/images/ic_chevronright.svg';
   static const String imgApple = 'assets/images/img_apple.png';
   static const String imgEmail = 'assets/images/img_email.png';
