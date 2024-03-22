ba4808aa3bcbf389d3017c422c28da7fd8832ad3
gim-yeong-geon <dudrjs145@naver.com>
2024-03-22 17:56:44 +0900

    스플래시 이미지 수정.

--- lib/app/ui/page/splash/splash_page.dart
+++ lib/app/ui/page/splash/splash_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 import 'package:physical_note/app/resources/resources.dart';
 import 'package:physical_note/app/ui/page/splash/splash_controller.dart';
@@ -16,20 +15,9 @@ class SplashPage extends GetView<SplashController> {
       isFullPage: false,
       safeStatusBar: false,
       safeNavigationBar: false,
-      child: Stack(
-        alignment: Alignment.topLeft,
-        children: [
-          Positioned(
-            top: 0,
-            child: SvgPicture.asset(Assets.backgroundTop),
-          ),
-          Center(child: SvgPicture.asset(Assets.logo),),
-          Positioned(
-            bottom: 0,
-            right: 0,
-            child: SvgPicture.asset(Assets.backgroundBottom),
-          ),
-        ],
+      child: Image.asset(
+        Assets.splashImage,
+        fit: BoxFit.fill,
       ),
     );
   }
