527c533d1bdd16fba0680c243000ee44264c0315
Conner <dudrjs145@naver.com>
2025-01-06 22:27:56 +0900

    feat: 생년월일 Localization 대응

--- lib/app/utils/regex/regex.dart
+++ lib/app/utils/regex/regex.dart
@@ -1,4 +1,5 @@
 import 'package:get/get.dart';
+import 'package:physical_note/app/utils/utils.dart';
 
 class Regex {
   static bool hasMatch(String? value, String pattern) {
@@ -11,8 +12,16 @@ class Regex {
   static bool isPhoneNumber(String s) => s.isPhoneNumber && s.length == 11;
 
   /// 생년월일 정규식
-  static bool isBirth(String s) => hasMatch(
+  static bool isBirth(String s) =>
+      LocalizationUtil.isKor ? _isKoBirth(s) : _isEnBirth(s);
+
+  static bool _isKoBirth(String s) => hasMatch(
         s,
         r'^(19[0-9][0-9]|20\d{2})(0[0-9]|1[0-2])(0[1-9]|[1-2][0-9]|3[0-1])$',
+      );
+
+  static bool _isEnBirth(String s) => hasMatch(
+        s,
+        r'^(0[1-9]|1[0-2])(0[1-9]|[1-2][0-9]|3[0-1])(19[0-9][0-9]|20\d{2})$',
       );
 }
